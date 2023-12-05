d427b11bc7a7ba8dd3a43ebf958c19a0952c3fae
unknown <subhan.akram2400@gmail.com>
2023-12-06 04:16:08 +0500

    promotion api added

--- controller/promotion/index.js
+++ controller/promotion/index.js
@@ -1,15 +1,21 @@
 const promotion = require("../../models/promotions")
-const mongoose =require("mongoose")
+const mongoose = require("mongoose")
 const ObjectId = mongoose.Types.ObjectId;
 const addPromotion = async (req, res) => {
 
     try {
-        const { menuId, restaurantId, discount, expireDate,openingAt } = req.body;
-        console.log(req.body);
-
-        const result = new promotion({ menuId:new ObjectId(menuId), restaurantId:new ObjectId(restaurantId), discount, expireDate,openingAt });
+        const { menuId, restaurantId, discount, expireDate, openingAt } = req.body;
+        const result = new promotion({ menuId: new ObjectId(menuId), restaurantId: new ObjectId(restaurantId), discount, expireDate, openingAt });
+       console.log("result===",result)
+        
         const data = await result.save();
-        res.status(200).send(data);
+        // const promotionInfo = await data.populate({ path: "restaurantId", select: "name" }).populate({ path: "menuId", select: "dish price img" });
+        const  promotionInfo = await promotion.populate(data, [
+            { path: "restaurantId", select: "name" },
+            { path: "menuId", select: "dish price img" }
+        ]);
+        console.log("promo info====", promotionInfo)
+        res.status(200).send(promotionInfo);
 
     } catch (error) {
         res.status(500).send(`Something went wrong ${error}`);
@@ -18,9 +24,9 @@ const addPromotion = async (req, res) => {
 
 const getPromotion = async (req, res) => {
     try {
-// i filter because in populate if id npot match it still return will null
-        const result = await promotion.find({}).populate({path:"restaurantId",select:"name"}).populate({path:"menuId",select:"dish price img"});
-        const filterRestaurants=result.filter(res=>res.restaurantId && res.menuId)
+        // i filter because in populate if id not match it still return will null
+        const result = await promotion.find({}).populate({ path: "restaurantId", select: "name" }).populate({ path: "menuId", select: "dish price img" });
+        const filterRestaurants = result.filter(res => res.restaurantId && res.menuId)
         res.status(200).send(filterRestaurants);
 
     } catch (error) {
@@ -28,7 +34,7 @@ const getPromotion = async (req, res) => {
     }
 }
 
-const featureRestaurant=async(req,res)=>{
+const featureRestaurant = async (req, res) => {
     try {
         const { menuId, restaurantId, discount, expireDate } = req.body;
         console.log(req.body);
