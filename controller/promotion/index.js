561190cd71db98321ad063c0b161ff225a859433
unknown <subhan.akram2400@gmail.com>
2023-12-06 04:54:38 +0500

    cod eupdated

--- controller/promotion/index.js
+++ controller/promotion/index.js
@@ -1,16 +1,18 @@
 const promotion = require("../../models/promotions")
+const Restaurant = require("../../models/restaurant")
 const mongoose = require("mongoose")
+
 const ObjectId = mongoose.Types.ObjectId;
 const addPromotion = async (req, res) => {
 
     try {
         const { menuId, restaurantId, discount, expireDate, openingAt } = req.body;
         const result = new promotion({ menuId: new ObjectId(menuId), restaurantId: new ObjectId(restaurantId), discount, expireDate, openingAt });
-       console.log("result===",result)
-        
+        console.log("result===", result)
+
         const data = await result.save();
         // const promotionInfo = await data.populate({ path: "restaurantId", select: "name" }).populate({ path: "menuId", select: "dish price img" });
-        const  promotionInfo = await promotion.populate(data, [
+        const promotionInfo = await promotion.populate(data, [
             { path: "restaurantId", select: "name" },
             { path: "menuId", select: "dish price img" }
         ]);
@@ -24,10 +26,46 @@ const addPromotion = async (req, res) => {
 
 const getPromotion = async (req, res) => {
     try {
-        // i filter because in populate if id not match it still return will null
         const result = await promotion.find({}).populate({ path: "restaurantId", select: "name" }).populate({ path: "menuId", select: "dish price img" });
-        const filterRestaurants = result.filter(res => res.restaurantId && res.menuId)
-        res.status(200).send(filterRestaurants);
+        // const pipeline = [
+        //     {
+        //         $lookup: {
+        //             from: 'restaurantmenus',
+        //             localField: 'menuId',
+        //             foreignField: '_id',
+        //             as: 'restaurant',
+        //         },
+        //     },
+        //     {
+        //         $unwind: '$restaurant',
+        //     },
+        //     {
+        //         $lookup: {
+        //             from: 'restaurantMenu',
+        //             localField: 'menuId',
+        //             foreignField: '_id',
+        //             as: 'menu',
+        //         },
+        //     },
+        //     {
+        //         $unwind: '$menu',
+        //     },
+        //     {
+        //         $project: {
+        //             _id: 0,
+        //             restaurant: '$restaurant.name',
+        //             menu: '$menu.dish',
+        //             discount: 1,
+        //             openingAt: 1,
+        //         },
+        //     },
+        // ];
+
+
+
+        // const resi = await promotion.aggregate(pipeline)
+        // console.log("res===", resi)
+        res.status(200).send(result);
 
     } catch (error) {
         res.status(500).send(`Something went wrong ${error}`);
