2f458f4f0bd18765bc41f32656d511045deb1060
unknown <subhan.akram2400@gmail.com>
2023-12-17 22:40:13 +0500

    personal user added in try this api

--- controller/tryThisMenu/index.js
+++ controller/tryThisMenu/index.js
@@ -24,77 +24,163 @@ const addTryMenuItems = async (req, res) => {
     }
 }
 
+// const getTryMenuItems = async (req, res) => {
+//     try {
+//         // const getReviews = await TryMenuItem.find({}).populate("menuId")
+//         const getReviews = await TryMenuItem.aggregate([{
+//             $lookup: {
+//                 from: "restaurantmenus",
+//                 localField: "menuId",
+//                 foreignField: "_id",
+//                 as: "menus"
+//             },
+//         },
+//         { $unwind: "$menus" },
+//         {
+//             $lookup: {
+//                 from: "menulikeitems",
+//                 localField: "menuId",
+//                 foreignField: "menuId",
+//                 as: "trueLikesCount"
+//             }
+//         },
+//         {
+//             $group:
+//             {
+//                 _id: "$menuId",
+//                 menuLikeItems: { $first: '$trueLikesCount.userLike' },
+//                 allUsersLikes: { $first: '$trueLikesCount' },
+//                 menus:{$first:'$menus'},
+//                 menuId:{$first:"$menus._id"},
+//                 menuInfo:{$first:"$menus.menuInfo"},
+//                 price:{$first:"$menus.price"},
+//                 dish:{$first:"$menus.dish"},
+//                 img:{$first:"$menus.img"},
+//                 restaurantId:{$first:"$menus.restaurantId"},
+//                 category:{$first:"$menus.category"},
+//             }
+//         },
+//         {
+//             $project: {
+//                 _id: 0,
+//                 menuId: 1,
+//                 restaurantId:1,
+//                 dish: 1,
+//                 price:1,
+//                 img:1,
+//                 menuInfo:1,
+//                 category:1,
+
+//                 // trueLikesCount:1
+//                 trueLikesCount: {
+//                     $reduce: {
+//                       input: "$menuLikeItems",
+//                       initialValue: 0,
+//                       in: {
+//                         $cond: {
+//                           if: { $eq: ["$$this", true] },
+//                           then: { $add: ["$$value", 1] },
+//                           else: "$$value"
+//                         }
+//                       }
+//                     }
+//                   },
+//                   allUsersLikes: 1,
+//             }
+//         }
+
+//         ])
+//         res.status(200).send(getReviews)
+//     } catch (err) {
+//         res.status(401).send(err)
+//     }
+// }
 const getTryMenuItems = async (req, res) => {
     try {
-        // const getReviews = await TryMenuItem.find({}).populate("menuId")
-        const getReviews = await TryMenuItem.aggregate([{
-            $lookup: {
-                from: "restaurantmenus",
-                localField: "menuId",
-                foreignField: "_id",
-                as: "menus"
-            },
-        },
-        { $unwind: "$menus" },
-        {
-            $lookup: {
-                from: "menulikeitems",
-                localField: "menuId",
-                foreignField: "menuId",
-                as: "trueLikesCount"
-            }
-        },
-        {
-            $group:
+        const userId = req.query.userId; // Assuming userId is obtained from the request query
+
+        const getReviews = await TryMenuItem.aggregate([
             {
-                _id: "$menuId",
-                menuLikeItems: { $first: '$trueLikesCount.userLike' },
-                allUsersLikes: { $first: '$trueLikesCount' },
-                menus:{$first:'$menus'},
-                menuId:{$first:"$menus._id"},
-                menuInfo:{$first:"$menus.menuInfo"},
-                price:{$first:"$menus.price"},
-                dish:{$first:"$menus.dish"},
-                img:{$first:"$menus.img"},
-                restaurantId:{$first:"$menus.restaurantId"},
-                category:{$first:"$menus.category"},
-            }
-        },
-        {
-            $project: {
-                _id: 0,
-                menuId: 1,
-                restaurantId:1,
-                dish: 1,
-                price:1,
-                img:1,
-                menuInfo:1,
-                category:1,
+                $lookup: {
+                    from: "restaurantmenus",
+                    localField: "menuId",
+                    foreignField: "_id",
+                    as: "menus",
+                },
+            },
+            { $unwind: "$menus" },
+            {
+                $lookup: {
+                    from: "menulikeitems",
+                    localField: "menuId",
+                    foreignField: "menuId",
+                    as: "trueLikesCount",
+                },
+            },
+            {
+                $group: {
+                    _id: "$menuId",
+                    menuLikeItems: { $first: '$trueLikesCount.userLike' },
+                    allUsersLikes: { $first: '$trueLikesCount' },
+                    menus: { $first: '$menus' },
+                    menuId: { $first: "$menus._id" },
+                    menuInfo: { $first: "$menus.menuInfo" },
+                    price: { $first: "$menus.price" },
+                    dish: { $first: "$menus.dish" },
+                    img: { $first: "$menus.img" },
+                    restaurantId: { $first: "$menus.restaurantId" },
+                    category: { $first: "$menus.category" },
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    menuId: 1,
+                    restaurantId: 1,
+                    dish:  1,
+                    price: 1,
+                    img: 1,
+                    menuInfo: 1,
+                    category: 1,
 
-                // trueLikesCount:1
-                trueLikesCount: {
-                    $reduce: {
-                      input: "$menuLikeItems",
-                      initialValue: 0,
-                      in: {
-                        $cond: {
-                          if: { $eq: ["$$this", true] },
-                          then: { $add: ["$$value", 1] },
-                          else: "$$value"
+                    trueLikesCount: {
+                        $reduce: {
+                            input: "$menuLikeItems",
+                            initialValue: 0,
+                            in: {
+                                $cond: {
+                                    if: { $eq: ["$$this", true] },
+                                    then: { $add: ["$$value", 1] },
+                                    else: "$$value",
+                                },
+                            },
+                        },
+                    },
+                    // allUsersLikes: 1,
+                    personalLike: {
+                        $filter: {
+                            input: '$allUsersLikes',
+                            as: 'like',
+                            cond: { $eq: ['$$like.userId',new mongoose.Types.ObjectId(userId)] }
                         }
-                      }
                     }
-                  },
-                  allUsersLikes: 1
-            }
-        }
+                },
+            },
+            {
+                $addFields: {
+                    personalLike: {
+                        $arrayElemAt: ['$personalLike', 0]
+                    }
+                }
+            },
+            
+        ]);
 
-        ])
-        res.status(200).send(getReviews)
+        res.status(200).send(getReviews);
     } catch (err) {
-        res.status(401).send(err)
+        res.status(401).send(err);
     }
-}
+};
 
 
 module.exports = {
